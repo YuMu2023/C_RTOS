63cd14514338180eb437355784ea6594baee5d1e
233 <481696912@qq.com>
2025-03-28 18:06:43 +0800

    commit

--- User/Serial.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SERIAL_H
-#define _SERIAL_H
-
-void Serial_Init(void);
-void Serial_SendByte(unsigned char b);
-void Serial_SendString(const char*str);
-void Serial_SendUnsigned(unsigned int num, int n);
-
-#endif//_SERIAL_H
